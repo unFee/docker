2ea783ffcd57a10ef311cb114c1219e77979c714
unFee <unfree45@gmail.com>
2021-12-29 17:55:06 +0900

    ログインしていない場合、ログイン画面へリダイレクト

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -22,5 +22,6 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::group(['middleware' => ['auth']], function() {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
