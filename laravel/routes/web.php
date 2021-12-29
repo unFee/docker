8f8ffab8acdc570a8635392280f1caa6a98f3ba7
unFee <unfree45@gmail.com>
2021-12-29 18:09:59 +0900

    ルーティングみやすく

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SampleController;
+use App\Http\Controllers\Auth\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,14 +15,23 @@ use App\Http\Controllers\SampleController;
 |
 */
 
+// 動作確認用
 Route::get('/redis_test', [SampleController::class, 'redis']);
 Route::get('/db_test', [SampleController::class, 'db']);
-
-Route::get('/', function () {
+Route::get('/test', function () {
     return view('welcome');
 });
 
-Auth::routes();
+// Auth::routes(); -- デフォルトのルーティング。一目で何を行なっているかを把握できないので書き直す。
+Route::redirect('/', '/login');
+Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [LoginController::class, 'login']);
+Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+
 Route::group(['middleware' => ['auth']], function() {
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+    // ユーザ登録処理 (招待制にするので、ログイン必須)
+    Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
+    Route::post('register', 'Auth\RegisterController@register');
 });
